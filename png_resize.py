f0ce1660419d49447a7b8ea15ef9595e99571cd6
Mayokara <62886973+Mayokara@users.noreply.github.com>
2021-01-29 16:23:59 +0900

    Add file :zap:

---
+++ png_resize.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+image_dir = pathlib.Path('./temp')
+png_path = list(image_dir.glob('**/*.png'))
+
+def resize_png(image_path, save_path):
+    img = Image.open(image_path)
+    img_resize = img.resize((img.width // 2, img.height // 2))
+    img_resize.save(str(save_path), "PNG")
+
+for i in png_path:
+    m = "./" + str(i)
+    n = './temp/' + str(i.stem) + '.png'
+    resize_png(m, n)
